a62862bf447a5315b76187c4ec67f82f9244c752
MaykerHM <hydeki8@gmail.com>
2021-01-23 22:46:43 -0300

    mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import trilhas from './data/db.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
